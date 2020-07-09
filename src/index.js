a17f1175bb0a383a720afee5bc56e86407a2f959
Jesser Susi <susi.jesser@.com>
2020-07-09 18:12:38 +1200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+
+ReactDOM.render(
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById("spotifyMain")
+);
